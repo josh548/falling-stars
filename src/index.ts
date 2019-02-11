1d2691a35f02ae5e6564dd53fe7c33b71568bb83
= <joshua.escamilla@selu.edu>
2019-02-10 23:31:35 -0600

    Scale canvas for high DPI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { Scene } from "./Scene";
 
-const canvas: HTMLCanvasElement = document.querySelector("canvas") as HTMLCanvasElement;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+canvas.width = window.innerWidth / window.devicePixelRatio;
+canvas.height = window.innerHeight / window.devicePixelRatio;
 
-const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+const computedWidth: number = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+const computedHeight: number = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+canvas.setAttribute("width", `${computedWidth * window.devicePixelRatio}px`);
+canvas.setAttribute("height", `${computedHeight * window.devicePixelRatio}px`);
+
+const context: CanvasRenderingContext2D = canvas.getContext("2d")!;
 const scene: Scene = new Scene(context);
 
 function animate(): void {
